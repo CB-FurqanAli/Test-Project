5fd0379462ba08d546ca817c8a19df9eee80b35a
CB-FurqanAli <furqan@cruisebrains.com>
2025-04-28 13:13:34 +0500

    add button for conflicts

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TextRightNow - Payments Dashboard</title>
+    <title>TextRightNow - Payments Dashboard (add button)</title>
     @vite('resources/css/app.css')
 
 </head>
